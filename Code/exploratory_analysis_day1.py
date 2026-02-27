0fa7ae1cb24f216f67b61ab36c8e6b434018ab9c
ogeikem <oge.ikem06@gmail.com>
2026-02-27 15:47:51 -0500

    Update exploratory_analysis_day1.py

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -9,11 +9,11 @@ data = pd.read_csv(r'C:\Users\ogeik\OneDrive\Desktop\BME 2315\Module-2-Epidemics
 #%%
 # Make a plot of the active cases over time
 
-plt.scatter(data['day'], data['active reported daily cases'])
-plt.title("Day vs Active Reported Daily Cases")
-plt.xlabel("Day")
-plt.ylabel("Active Reported Daily Cases")
-plt.show()
+plt.scatter(data['day'], data['active reported daily cases']) # plotting the raw data points as a scatter plot
+plt.title("Day vs Active Reported Daily Cases") #adding a title to the plot
+plt.xlabel("Day") # adding a label to the x-axis
+plt.ylabel("Active Reported Daily Cases") # adding a label to the y-axis
+plt.show() # showing the plot
 
 # 1. The first thing I notice about the graphs is that the number of active reported daily cases increases exponentially after staying flat for a decent bit in the first few days. The suggests that the virus is spreading slowly at first and then has a very exponential increase in new cases, as opposed to linear growth.
 # 2. Some ways we could measure how quickly the disease is spreading is by calculating the growth rate (the percentage of increase in new cases each day), the amount of time it takes cases to double, the slope of the curve at its steepest, and average number of people one infected person transmits the virus to R₀).
